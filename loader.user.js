1cf9c2114ef71261e97d22ce8cb6cf651a393d2c
aaaaadxdcv <a.h@hotmail.no>
2015-08-15 07:38:58 +0200

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -1,4 +1,19 @@
 // ==UserScript==
+// @name         ph4nt0m_pwn Agariomods modscript
+// @namespace	 ph4nt0m_pwn Agariomods modscript
+// @version      1.9.9
+// @description  ph4nt0m_pwn Agariomods modscript
+// @author       ph4nt0m_pwn
+// @match        http://agar.io/
+// @match        http://agar.io/#*
+// @match        http://agar.io/?*
+// @grant        none
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.src = document.location.protocol+"//omniphaser.96.lt/ph4nt0m_pwn-agariomods.js";
+(document.body || document.head || document.documentElement).appendChild(script);
+// ==UserScript==
 // @name        AposLoader
 // @namespace   AposLoader
 // @description Grabs latest versions of the bot scripts automatically.
